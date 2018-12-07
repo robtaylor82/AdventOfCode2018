6bfb1c150800045d476e82fa6a4af54f861f0533
Rob Taylor <rob@rtaylor.info>
2018-12-07 11:09:43 +0000

    Day 06 (part 2) solution.

--- day06.js
+++ day06.js
@@ -47,6 +47,40 @@ Locations shown as . are equally far from two or more coordinates, and so they d
 In this example, the areas of coordinates A, B, C, and F are infinite - while not shown here, their areas extend forever outside the visible grid. However, the areas of coordinates D and E are finite: D is closest to 9 locations, and E is closest to 17 (both including the coordinate's location itself). Therefore, in this example, the size of the largest area is 17.
 
 What is the size of the largest area that isn't infinite?
+
+Your puzzle answer was 4215.
+
+--- Part Two ---
+On the other hand, if the coordinates are safe, maybe the best you can do is try to find a region near as many coordinates as possible.
+
+For example, suppose you want the sum of the Manhattan distance to all of the coordinates to be less than 32. For each location, add up the distances to all of the given coordinates; if the total of those distances is less than 32, that location is within the desired region. Using the same coordinates as above, the resulting region looks like this:
+
+..........
+.A........
+..........
+...###..C.
+..#D###...
+..###E#...
+.B.###....
+..........
+..........
+........F.
+In particular, consider the highlighted location 4,3 located at the top middle of the region. Its calculation is as follows, where abs() is the absolute value function:
+
+Distance to coordinate A: abs(4-1) + abs(3-1) =  5
+Distance to coordinate B: abs(4-1) + abs(3-6) =  6
+Distance to coordinate C: abs(4-8) + abs(3-3) =  4
+Distance to coordinate D: abs(4-3) + abs(3-4) =  2
+Distance to coordinate E: abs(4-5) + abs(3-5) =  3
+Distance to coordinate F: abs(4-8) + abs(3-9) = 10
+Total distance: 5 + 6 + 4 + 2 + 3 + 10 = 30
+Because the total distance to all coordinates (30) is less than 32, the location is within the region.
+
+This region, which also includes coordinates D and E, has a total size of 16.
+
+Your actual region will need to be much larger than this example, though, instead including all locations with a total distance of less than 10000.
+
+What is the size of the region containing all locations which have a total distance to all given coordinates of less than 10000?
 */
 
 'use strict';
@@ -179,6 +213,46 @@ grid = clearGridOfAllInfinateAreas(grid);
 var result = findLargestArea(grid);
 
 console.log('Largest non-infinate area is ' + result);
+
+// Part 2
+
+function populateGridWithSumOfDistanceToAllCoordinates(grid, coordinates, maxDistance){
+    for(var y=0; y < grid.length; y++){
+        for(var x=0; x < grid[y].length; x++){
+            var totalDistance = 0;
+            for(var i=0; i < coordinates.length; i++){
+                totalDistance += calculateManhattanDistance(x,y,coordinates[i]);
+            }
+
+            if(totalDistance < maxDistance){
+                grid[y][x] = totalDistance;
+            }
+            else{
+                grid[y][x] = '.';
+            }
+        }
+    }
+
+    return grid;
+}
+
+grid = buildGrid(coordinates);
+grid = populateGridWithSumOfDistanceToAllCoordinates(grid, coordinates, 10000);
+var sizeOfSafeRegion = 0;
+grid.forEach(y=> y.forEach(x=> {if(x !== '.') sizeOfSafeRegion++;}));
+
+console.log('There safe region has an area of ' + sizeOfSafeRegion + ' cells.');
+
+describe('populateGridWithSumOfDistanceToAllCoordinates', function(){
+    it('sum the total distance to all coordinates on each square, and discard all squares over 32', function(){
+       var coordinates = loadInputAndAssignIds('1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9');
+       var grid = buildGrid(coordinates);
+       var results = populateGridWithSumOfDistanceToAllCoordinates(grid, coordinates, 32);
+       results[0][0].should.be.equal('.');
+       results[3][2].should.be.equal('.');
+       results[3][4].should.not.be.equal('.');
+    });
+});
 
 describe('find largest area', function(){
     it('the largest number of grid squares with the same id should be return', function(){
